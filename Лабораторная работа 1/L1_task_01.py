d1bc73df882dade3780c611bce804d440828612e
DenisUser01001 <149439693+DenisUser01001@users.noreply.github.com>
2023-11-16 19:09:36 +0300

    Update L1_task_01.py
    Correction # 1 after teatchers request.

--- Лабораторная работа 1/L1_task_01.py
+++ Лабораторная работа 1/L1_task_01.py
@@ -1,8 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-new_list = numbers[:4] + numbers[5:]
-average_of_new_list = sum(new_list)/(len(new_list)+1)
-numbers[4] = average_of_new_list
+missing_index = 4  # Определение индекса элемента, который требуется заменить.
+
+new_list = numbers[:missing_index] + numbers[missing_index+1:]
+print(len(new_list))
+print(len(numbers))
+average_of_new_list = sum(new_list)/(len(numbers))
+numbers[missing_index] = average_of_new_list
 
 print("Измененный список:", numbers)
