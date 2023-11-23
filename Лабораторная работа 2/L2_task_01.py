74906870cae8bab9c3fda705b61b69b1c739318f
DenisUser01001 <149439693+DenisUser01001@users.noreply.github.com>
2023-11-23 23:14:43 +0300

    Update L2_task_01.py

--- Лабораторная работа 2/L2_task_01.py
+++ Лабораторная работа 2/L2_task_01.py
@@ -5,12 +5,14 @@ increase = 0.05  # Ежемесячный рост цен
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
-budget = money_capital + salary - spend
-mounths_of_prosperity = 0
+months_of_prosperity = 0
 
-while budget >= 0:
-    mounths_of_prosperity += 1
+while True:
+    income_difference = spend - salary
+    if income_difference > money_capital:
+        break
+    money_capital -= income_difference
+    months_of_prosperity += 1
     spend *= 1 + increase   # Расчёт методом умножения на коэффициент больше 1 (так как increase задан в виде "0,05")
-    budget = budget + salary - spend
 
-print("Количество месяцев, которое можно протянуть без долгов:", mounths_of_prosperity)
+print("Количество месяцев, которое можно протянуть без долгов:", months_of_prosperity)
